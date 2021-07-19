e8aa22d36c0648d72e5eab36c6b3cc11d5add362
David Duarte <licorne@google.com>
2021-07-19 08:15:02 +0000

    mmi: Parse mmi and convert ids
    Change-Id: I595b3e6592b71bccd5051ac14b8a65ae566d7a82

--- libpts/src/lib.rs
+++ libpts/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod hci;
 pub mod installer;
+pub mod mmi;
 pub mod pts;
 pub mod wine;
---
+++ libpts/src/mmi.rs
@@ -0,0 +1,39 @@
+pub fn id_to_mmi(profile: &str, id: u32) -> Option<&'static str> {
+    include!("../data/mmi_ids.inc.rs")
+}
+
+pub fn parse(description: &str) -> Option<(&str, &str, &str, &str)> {
+    let description = description.strip_prefix("{")?;
+    let (header, description) = description.split_once("}")?;
+    let (id, header) = header.split_once(",")?;
+    let (test, suite) = header.split_once(",")?;
+
+    Some((id, test, suite, description))
+}
+
+#[cfg(test)]
+mod test {
+    use super::id_to_mmi;
+    use super::parse;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            parse("{1002,A2DP/SNK/AS/BV-01-I,A2DP}If necessary, take action ..."),
+            Some((
+                "1002",
+                "A2DP/SNK/AS/BV-01-I",
+                "A2DP",
+                "If necessary, take action ..."
+            )),
+        );
+    }
+
+    #[test]
+    fn test_id_to_mmi() {
+        assert_eq!(
+            id_to_mmi("A2DP", 1002),
+            Some("TSC_AVDTP_mmi_iut_accept_connect")
+        )
+    }
+}

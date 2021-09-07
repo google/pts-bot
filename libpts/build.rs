c63b28bfdaa146b24515ad3584309876293c2f6f
Charlie Boutier <charliebout@google.com>
2021-09-07 14:52:18 +0000

    all: Add Cargo.toml and build.rs
    Change-Id: I0ca62a8de0f290452f7160d72e748396be4ccf07

---
+++ libpts/build.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::process::Command;
+
+fn main() {
+    let out_dir: String = env::var("OUT_DIR").unwrap();
+
+    // Build ETSManager for wine
+    let status = Command::new("winebuild")
+        .arg("--def")
+        .arg("-E")
+        .arg("./server/ETSManager.spec")
+        .arg("-o")
+        .arg(format!("{}/libETSManager.def", &out_dir))
+        .status()
+        .expect("Failed to winebuild");
+    assert!(status.success());
+
+    let status = Command::new("winegcc")
+        .arg("-m32")
+        .arg("./server/main.c")
+        .arg("-L")
+        .arg(&out_dir)
+        .arg("-lETSManager")
+        .arg("-o")
+        .arg(format!("{}/server.exe.so", &out_dir))
+        .status()
+        .expect("Failed to winegcc");
+    assert!(status.success());
+
+    println!("cargo:rustc-env=SERVER_PATH={}/server.exe.so", &out_dir);
+    println!("cargo:rustc-env=INSTALLER_PATH=../pts_setup_8_0_3.exe")
+}

8ba014f5ba2b3e69b2cd7437e4e501c8dd2328cb
Charlie Boutier <charliebout@google.com>
2021-11-10 13:49:41 +0000

    xml model: Add non_contiguous_seq_element to parser
    Bug: 205559967
    Change-Id: I94add939343b3ea49d96d7e95b5383ceb0a7ae3a

--- libpts/src/xml_model/mod.rs
+++ libpts/src/xml_model/mod.rs
@@ -4,7 +4,7 @@ pub mod picsx;
 pub mod pixitx;
 
 use serde::Deserialize;
-use serde_xml_rs::from_str;
+use serde_xml_rs::Deserializer;
 
 use crate::installer::PTS_PATH;
 use crate::wine::Wine;
@@ -35,7 +35,10 @@ pub trait XMLModel<'a>: Deserialize<'a> {
         let content: String = read_to_string(path.clone()).map_err(|err| {
             Error::FileNotFound(err, String::from(path.to_str().unwrap_or("Unknown")))
         })?;
-        let value: Self = from_str(&content).map_err(|err| Error::ParseFailed(err))?;
+
+        let mut de =
+            Deserializer::new_from_reader(content.as_bytes()).non_contiguous_seq_elements(true);
+        let value: Self = Self::deserialize(&mut de).map_err(|err| Error::ParseFailed(err))?;
         Ok(value)
     }
 }

f4ba1ae66dfdae67e1589840490dc825f9d71307
David Duarte <licorne@google.com>
2021-11-10 15:58:48 +0000

    bd_addr: Implement an alternate display format
    The alternate format is AABBCCDDEEFF instead of
    aa:bb:cc:dd:ee:ff
    
    This format is used by the PTS
    
    Change-Id: I9a41e8481334219105b84ee6f9235c61d6f93077

--- libpts/src/bd_addr.rs
+++ libpts/src/bd_addr.rs
@@ -18,11 +18,19 @@ impl BdAddr {
 
 impl fmt::Display for BdAddr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
-            self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5]
-        )
+        if f.alternate() {
+            write!(
+                f,
+                "{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}",
+                self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5]
+            )
+        } else {
+            write!(
+                f,
+                "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
+                self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5]
+            )
+        }
     }
 }
 

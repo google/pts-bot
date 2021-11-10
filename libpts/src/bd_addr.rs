22d2c396c4a42e8a7ef229d90834e6858e8e1fff
David Duarte <licorne@google.com>
2021-11-10 16:33:19 +0000

    bd_addr: Parse addresses with colons
    Change-Id: I40190655fa4248cacd47812eaa1e8d516841712f

--- libpts/src/bd_addr.rs
+++ libpts/src/bd_addr.rs
@@ -1,9 +1,12 @@
 use serde::{de, Deserialize, Deserializer};
+use thiserror::Error;
+
 use std::fmt;
+use std::num::ParseIntError;
 use std::ops::Deref;
 use std::str::FromStr;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 pub struct BdAddr([u8; 6]);
 
 impl BdAddr {
@@ -34,17 +37,36 @@ impl fmt::Display for BdAddr {
     }
 }
 
+#[derive(PartialEq, Debug, Error)]
+pub enum ParseBdAddrError {
+    #[error("Invalid Byte in Address ({0})")]
+    InvalidByte(#[from] ParseIntError),
+    #[error("Unknown Address Format")]
+    UnknownFormat,
+}
+
 impl FromStr for BdAddr {
-    type Err = std::num::ParseIntError;
+    type Err = ParseBdAddrError;
     fn from_str(src: &str) -> Result<BdAddr, Self::Err> {
-        Ok(Self([
-            u8::from_str_radix(&src[0..2], 16)?,
-            u8::from_str_radix(&src[2..4], 16)?,
-            u8::from_str_radix(&src[4..6], 16)?,
-            u8::from_str_radix(&src[6..8], 16)?,
-            u8::from_str_radix(&src[8..10], 16)?,
-            u8::from_str_radix(&src[10..12], 16)?,
-        ]))
+        match src.len() {
+            12 => Ok(Self([
+                u8::from_str_radix(&src[0..2], 16)?,
+                u8::from_str_radix(&src[2..4], 16)?,
+                u8::from_str_radix(&src[4..6], 16)?,
+                u8::from_str_radix(&src[6..8], 16)?,
+                u8::from_str_radix(&src[8..10], 16)?,
+                u8::from_str_radix(&src[10..12], 16)?,
+            ])),
+            17 if src.chars().skip(2).step_by(3).all(|c| c == ':') => Ok(Self([
+                u8::from_str_radix(&src[0..2], 16)?,
+                u8::from_str_radix(&src[3..5], 16)?,
+                u8::from_str_radix(&src[6..8], 16)?,
+                u8::from_str_radix(&src[9..11], 16)?,
+                u8::from_str_radix(&src[12..14], 16)?,
+                u8::from_str_radix(&src[15..17], 16)?,
+            ])),
+            _ => Err(ParseBdAddrError::UnknownFormat),
+        }
     }
 }
 
@@ -63,5 +85,26 @@ impl<'de> Deserialize<'de> for BdAddr {
     {
         let s = String::deserialize(deserializer)?;
         FromStr::from_str(&s).map_err(de::Error::custom)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::BdAddr;
+
+    #[test]
+    fn test_from_str_with_colons() {
+        assert_eq!(
+            "11:22:33:44:55:66".parse(),
+            Ok(BdAddr::new([0x11, 0x22, 0x33, 0x44, 0x55, 0x66]))
+        );
+    }
+
+    #[test]
+    fn test_from_str_without_colons() {
+        assert_eq!(
+            "112233445566".parse(),
+            Ok(BdAddr::new([0x11, 0x22, 0x33, 0x44, 0x55, 0x66]))
+        );
     }
 }

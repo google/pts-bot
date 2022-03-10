55f16e74a553eb2f7686ebb5672fc652e669c0f3
Charlie Boutier <charliebout@google.com>
2022-03-10 13:39:17 +0000

    pts2json: Remove 'sys' and use 'argparse' instead
    Change-Id: I267158d4b2d9f423cb07501288f6d4c170e7fb20

--- script/pts2json.py
+++ script/pts2json.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 import json
+from pathlib import Path
 from xml.dom import minidom
 
-_file_name = "config.json"
-
-def to_json(ics_xml_path: str):
-    global _file_name
-    if len(sys.argv) > 2:
-        _file_name = sys.argv[2]
-    config_file = open(_file_name, "w")
+def pts2json(pts_file: Path, output: Path):
     TSPC = {'ics': {}, 'ixit': {}}
     ics = TSPC['ics']
     ixit = TSPC['ixit']
     ixit['default'] = {}
-    ics_xml = minidom.parse(ics_xml_path)
+    ics_xml = minidom.parse(str(pts_file))
     profiles = ics_xml.getElementsByTagName('profile')
     for profile in profiles:
         profile_name = profile.getElementsByTagName('name')[0].firstChild.nodeValue
@@ -26,15 +21,22 @@ def to_json(ics_xml_path: str):
             row = item.getElementsByTagName('row')[0].firstChild.nodeValue
             tspc = f"TSPC_{profile_name}_{table}_{row}"
             ics[tspc] = True
-    json.dump(TSPC, config_file, indent=2)
-    print(f'success: {_file_name}')
+    with open(output, "w") as config_file:
+        json.dump(TSPC, config_file, indent=2)
+        print(f'success: {output}')
 
 def main():
-    if len(sys.argv) > 1:
-        print(f'pts file: {sys.argv[1]}')
-        to_json(sys.argv[1])
-    else:
-        print("pts file needed as argument")
+    parser = argparse.ArgumentParser(description="Parse pts file to json")
+    parser.add_argument('--pts_file',
+                        type=Path,
+                        required=True,
+                        help='pts config file generated with Launch Studio')
+    parser.add_argument('--output',
+                        type=Path,
+                        required=False,
+                        default=Path('pts_config.json'),
+                        help='Output name for the pts config file')
+    pts2json(**vars(parser.parse_args()))
 
 if __name__ == "__main__":
   main()
